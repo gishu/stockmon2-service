db036ade54caf711a3629da2af7a0940ad21b248
Gishu Pillai <gishu@hotmail.com>
2016-07-13 10:05:46 +0530

    try 2 simultaneous accounts

--- app/testDrive.js
+++ app/testDrive.js
@@ -12,10 +12,10 @@ var util = require('util');
 var async = require('async');
 var log = require('debug')('app')
 
-var dbPath = './stockmon.sqlite';
+var dbPath = './stockmon2.sqlite';
 try {
     fs.accessSync(dbPath, fs.F_OK);
-    fs.unlinkSync(dbPath);
+    //fs.unlinkSync(dbPath);
 }
 catch (e) {
     if (!(e.code === 'ENOENT')) {
@@ -28,50 +28,72 @@ var database = getDatabase(dbPath);
 var mapper = getAccountMapper(database);
 var snapshotMapper = getSnapshotMapper(database);
 
-var istream = fs.createReadStream('./app/trades.csv');
-
-
+var istream = fs.createReadStream('./app/trades_master.csv');
 async.waterfall([
     cb => {
-        log('Reading csv..');
-        parse(istream, (err, results) => {
-            log('Loading trades...');
-            var a = account.create('Gishu');
-            a.register(results.trades);
-            a.addDividends(results.dividends);
-            cb(err, a);
-        });
+        mapper.load(1, (err, account) => cb(err, account))
     },
-    (account, cb) => {
-        log('Saving account.');
-        mapper.save(account, (err, account) => {
-            cb(null, account);
-        });
+    (acc, cb) => {
+        acc.getHoldings((err, holdings) => cb(err, holdings))
     },
-    (account, cb) => {
-        log('Getting snapshots');
-        account.getAnnualStmts((err, snapshots) => {
-            if (err) {
-                cb(err, null);
-                return;
-            }
-            cb(null, account.id(), snapshots);
-        });
-    },
-    (accountId, snapshots, cb) => {
-        log('Saving snapshots..');
-        snapshotMapper.saveSnapshots(accountId, snapshots, (err) => {
-            cb(err, accountId);
-        })
+    (holdings, cb) => {
+        database.close(err => cb(err, holdings));
     }
 ],
-    (err, id) => {
-        if (!err) {
-            log('Yay! saved snapshots for ' + id);
-        } else {
-            log('Potti' + err);
-        }
-        database.close(err => log('DB Close: ' + err));
-    }
-);
+    (err, holdings) => {
+        console.log('%j', holdings);
+    });
+
+// async.waterfall([
+//     cb => {
+//         log('Reading csv..');
+//         parse(istream, (err, results) => {
+//             log('Loading trades...');
+//             var a = account.create('gishu');
+//             a.register(results.trades);
+//             a.addDividends(results.dividends);
+//             cb(err, a);
+//         });
+//     },
+//     (account, cb) => {
+//         log('Saving account.');
+//         mapper.save(account, (err, account) => {
+//             cb(null, account);
+//         });
+//     },
+//     (account, cb) => {
+//         log('Getting snapshots');
+//         account.getAnnualStmts((err, snapshots) => {
+//             if (err) {
+//                 cb(err, null);
+//                 return;
+//             }
+//             cb(null, account.id(), snapshots);
+//         });
+//     },
+//     (accountId, snapshots, cb) => {
+//         log('Saving snapshots..');
+//         //snapshots = _.initial(snapshots);
+//         if (snapshots.length > 0) {
+//             snapshotMapper.saveSnapshots(accountId, snapshots, (err) => {
+//                 cb(err, accountId);
+//             })
+//         } else {
+//             cb(null, accountId);
+//         }
+
+//     },
+//     (accountId, cb) => {
+//         database.close(err => cb(err, accountId));
+//     }
+// ],
+//     (err, id) => {
+//         if (!err) {
+//             log('Yay! saved snapshots for ' + id);
+//         } else {
+//             log('Potti' + err);
+//         }
+//         //database.close(err => log('DB Close: ' + err));
+//     }
+// );
 
