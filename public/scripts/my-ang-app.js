7bceb02dd287b495f3a863cf83ce7f8a90d00393
Gishu Pillai <gishu@hotmail.com>
2016-09-11 22:10:07 +0530

    fix hardcoded accountId

--- public/scripts/my-ang-app.js
+++ public/scripts/my-ang-app.js
@@ -1,24 +1,29 @@
 
-
 var app = angular.module('myAngApp', ['ngTouch', 'ui.grid', 'myAngServices']);
 
-app.controller('MainCtrl', ['$scope', 'TradeService', 'uiGridConstants', function ($scope, tradesService, gridConstants) {
-    $scope.gridViewModel = {
-        enableFiltering: true,
+app.controller('MainCtrl', ['$scope', '$location', 'TradeService', 'uiGridConstants',
+    function ($scope, $location, tradesSvc, gridConstants) {
+        $scope.gridViewModel = {
+            enableFiltering: true,
 
-        columnDefs: [
-            { name: 'Date', field: 'date', sort: {direction: gridConstants.DESC, priority:0} },
-            { name: 'Stock', field: 'stock' },
-            { name: 'Qty', field: 'qty' },
-            { name: 'Price', field: 'price' },
-            { name: 'B/S', field: 'type' }
-        ],
-        data: []
-    };
+            columnDefs: [
+                { name: 'Date', field: 'date', sort: { direction: gridConstants.DESC, priority: 0 } },
+                { name: 'Stock', field: 'stock' },
+                { name: 'Qty', field: 'qty' },
+                { name: 'Price', field: 'price' },
+                { name: 'B/S', field: 'type' }
+            ],
+            data: []
+        };
 
-    tradesService.getTrades(1).then(trades => {
-        $scope.gridViewModel.data = trades;  
-    });
-    
-}]);
+        var accountId = $location.absUrl().match(/accounts\/(\d+)/)[1];
+
+        tradesSvc.getTrades(_.toInteger(accountId)).then(
+            trades => {
+                $scope.gridViewModel.data = trades;
+            },
+            err => console.error(err));
+
+    }]
+);
 
